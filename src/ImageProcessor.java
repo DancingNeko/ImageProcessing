c8a8bd2204c3533dc130673bb936f9bc3e28b82e
Daniel Feng <daniel@Daniels-MacBook-Air-3.local>
2022-10-03 22:28:31 -0400

    temp

--- src/ImageProcessor.java
+++ src/ImageProcessor.java
@@ -70,7 +70,6 @@ public class ImageProcessor {
                     }
                 }
                 grey = Math.abs(grey);
-                //grey = (grey / 255.0) * (grey / 255.0) * 255;
                 int rgb = 255;
                 rgb = (rgb << 8) + (int) grey;
                 rgb = (rgb << 8) + (int) grey;
@@ -81,30 +80,44 @@ public class ImageProcessor {
         return out;
     }
 
+    public static double[][] applySobelKernel(double[][] kernel,
+            BufferedImage img) {
+        double[][] out = new double[img.getWidth()][img.getHeight()];
+        for (int i = kernel.length; i < img.getWidth() - kernel.length; i++) {
+            for (int j = kernel.length; j < img.getHeight()
+                    - kernel.length; j++) {
+                double grey = 0;
+                for (int x = 0; x < kernel.length; x++) {
+                    for (int y = 0; y < kernel.length; y++) {
+                        int argb = img.getRGB(i + x - kernel.length / 2,
+                                j + y - kernel.length / 2);
+                        int blue = (argb) & 0xFF;
+                        int green = (argb >> 8) & 0xFF;
+                        int red = (argb >> 16) & 0xFF;
+                        grey += kernel[x][y] * (red + green + blue) / 3.0;
+                    }
+                }
+                grey = Math.abs(grey);
+                out[i][j] = grey;
+            }
+        }
+        return out;
+    }
+
     public static BufferedImage sobel(BufferedImage img) {
         double[][] Gx = { { 1, 0, -1 }, { 2, 0, -2 }, { 1, 0, -1 } };
         double[][] Gy = { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };
-        BufferedImage xProcessed = applyKernel(Gx, img);
-        BufferedImage yProcessed = applyKernel(Gy, img);
-
+        double[][] xProcessed = applySobelKernel(Gx, img);
+        double[][] yProcessed = applySobelKernel(Gy, img);
         BufferedImage out = new BufferedImage(img.getWidth(), img.getHeight(),
                 BufferedImage.TYPE_BYTE_GRAY);
         for (int x = 0; x < img.getWidth(); x++) {
             for (int y = 0; y < img.getHeight(); y++) {
-                int xRGB = xProcessed.getRGB(x, y);
-                int blue = (xRGB) & 0xFF;
-                int green = (xRGB >> 8) & 0xFF;
-                int red = (xRGB >> 16) & 0xFF;
-                double greyXScaled = red / 255.0;
-                int yRGB = yProcessed.getRGB(x, y);
-                blue = (yRGB) & 0xFF;
-                green = (yRGB >> 8) & 0xFF;
-                red = (yRGB >> 16) & 0xFF;
-                double greyYScaled = (red + green + blue) / 3.0 / 255.0;
+                double greyXScaled = xProcessed[x][y] / 255.0;
+                double greyYScaled = yProcessed[x][y] / 255.0;
                 double grey = Math.sqrt(
                         greyXScaled * greyXScaled + greyYScaled * greyYScaled)
                         * 255;
-                
                 int rgb = 255;
                 rgb = (rgb << 8) + (int) grey;
                 rgb = (rgb << 8) + (int) grey;
